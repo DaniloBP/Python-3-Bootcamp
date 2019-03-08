99574ce7d085df390e510c4bc99d6e13ddd0b7c4
Danilo Batista <pereira.b.danilo@gmail.com>
2019-03-08 18:38:57 -0300

    Truthiness in Python

--- Projects/mileage.py
+++ Projects/mileage.py
@@ -3,5 +3,10 @@ kms = input()
 miles = float(kms)/1.60934
 miles = round(miles, 2)
 
-#print(f"OK, you ran {miles} miles" .format(miles))
-print(f"OK, {kms} kilometers is equal to {miles} miles" )
+#print("OK, you ran {miles} miles" .format(miles))
+if miles <= 10.0:
+	print(f"\nOK, good run.\t({kms} kilometers is equal to {miles} miles)\n" )
+elif miles <= 15.0:
+	print(f"\nOK, I see you! Great run, my brother\\sister.\t({kms} kilometers is equal to {miles} miles)\n" )
+else:
+	print(f"\nDamn, son!!! You went full Kenian mode today, my man.\t({kms} kilometers is equal to {miles} miles)\n" )
---
+++ truthiness.py
@@ -0,0 +1,6 @@
+color = input("What's your favorite color?")
+
+if color:
+	print(f"\n{color} is my favorite color too!\n")
+else:
+	print("\nTry to type something in next time, genius!\n")
