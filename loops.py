9fa5652711885dde4f4f1e6397eef03c6ce3401d
Danilo Batista <pereira.b.danilo@gmail.com>
2019-03-11 01:29:53 -0300

    Starting with loops

---
+++ loops.py
@@ -0,0 +1,8 @@
+num = input("How many times do I have to tell you this?\t")
+
+if num:
+	num = int(num)
+	for x in range(num):
+		print("\nGO CLEAN YOUR SHIT!")
+else:
+	print("\nType some integer in, dumbass!!\n")
---
+++ ranges.py
@@ -0,0 +1,13 @@
+#for num in range(10):
+#	print(num)
+
+# It never includes the last number.
+#for num in range(1, 10):
+#	print(num)
+
+# The third parameter informs the step that the range must apply.
+#for num in range(1, 20, 2):
+#	print(num)
+
+for num in range(10, 1, -2):
+	print(num)
