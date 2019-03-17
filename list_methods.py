e129ec1959e13b162c3dd58fc3a541506221a0c7
Danilo Batista <pereira.b.danilo@gmail.com>
2019-03-17 03:38:38 -0300

    Updates in list_methods

--- list_methods.py
+++ list_methods.py
@@ -19,3 +19,29 @@ print(f"\nList after the first .insert() a list: {numbers}\n")
 
 numbers.insert(3, "Third item")
 print(f"\nList after the firsecond .insert() a list: {numbers}\n")
+
+colors = ['blue', 'red', 'yellow', 'green']
+print(colors)
+
+colors.clear()   # Deletes all items in a list
+print(colors)
+
+colors = ['blue', 'red', 'yellow', 'green']
+
+last_item = colors.pop()		# If no parameter is passed, removes and returns the last item in the list.
+
+print(f'\nThe removed item was -->  {last_item}\n')
+
+colors.pop(0)		# Removes the item at the given index.
+
+print(f'\n{colors}\n')
+
+colors.remove('red')		# Removes the first ocurrence of the passed element from the list.
+
+print(f'\n{colors}\n')
+
+index = numbers.index(4)
+
+print(f'\n{index}\n')
+
+
