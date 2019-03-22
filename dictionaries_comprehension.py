bc9fffa0461c14e589eab8811433fb1c75ac48af
Danilo Batista <pereira.b.danilo@gmail.com>
2019-03-22 01:43:10 -0300

    Dictionary Comprehension

---
+++ dictionaries_comprehension.py
@@ -0,0 +1,29 @@
+
+# numbers = dict( first = 1, second = 2, third = 3, fourth = 4, fifth = 5)
+
+# squares = { key : value ** 2 for key, value in numbers.items() }
+# print(squares)
+
+# new_dict = { num : num ** 2 for num in [6,7,8,9,10] }
+# print(new_dict)
+
+# str1 = "ABC"
+# str2 = "123"
+# new_dict = { str1[i] : str2[i] for i in range(0, len(str1)) }
+# print(new_dict)
+
+# list1 = ['a', 'b', 'c', 'd', 'e']
+# list2 = [1,2,3,4,5]
+# new_dict = { list1[i] : list2[i] / 2 for i in range(0, len(list2)) }
+# print(new_dict)
+
+# new_dict = { num : ("even" if num % 2 == 0 else "odd") for num in range(1,11) }		# Conditional logic in dictionary comprehension can be applied to keys and values.
+# print(new_dict)
+
+# EXERCISES
+
+person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
+
+# use the person variable in your answer
+answer = { }
+print(answer)
