61ac7ccf38f533fec386a1eb58950465f60c9883
Danilo Batista <pereira.b.danilo@gmail.com>
2019-04-02 16:02:06 -0300

    lambda

---
+++ lambda.py
@@ -0,0 +1,12 @@
+
+def add(a, b):
+	return a + b
+
+print( add(10, 35) )	# 45
+
+add2 = lambda a, b: a + b		# It returns automaticly
+
+multiply = lambda d, e, f: d * e * f
+
+print( add2(10, 35) )	# 45
+print( multiply(2, 3, 4))	# 24
--- unpacking.py
+++ unpacking.py
@@ -62,6 +62,5 @@ def calculate(**kwargs):
 
     return message + " " + str(result)
 
-
 print( calculate(make_float=False, operation='add', message='You just added', first=2, second=4) )	# "You just added 6"
 print( calculate(make_float=True, operation='divide', first=3.5, second=5) )	 # "The result is 0.7"
