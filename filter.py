0bd94e56c5a5c75dcaf2cc49d066428515a9cb89
Danilo Batista <pereira.b.danilo@gmail.com>
2019-04-03 19:58:30 -0300

    Filter updates

--- filter.py
+++ filter.py
@@ -2,6 +2,16 @@
 nums = range(0,21)
 
 odds = filter( lambda num : num % 2 != 0, nums)
-print(odds)
+# print(odds)
+# print(list(odds))
 
-print(list(odds))
+users = [ 
+		  { "user_name" : "MChammer", "shopcart" : ["HeadPhone Beats", "Mouse Microsoft"]},
+		  { "user_name" : "Paul_George", "shopcart" : ["Basket Ball NIKE", "T-shirt 76ers"]},
+		  { "user_name" : "KittyKat", "shopcart" : []},
+		  { "user_name" : "Rainbow_Killer", "shopcart" : ["Pro Bow", "Silver Arrows"]},
+		  { "user_name" : "GodsSlayer", "shopcart" : []}
+		]
+
+buyers = list( filter( lambda user : not user["shopcart"], users) )
+print(buyers)
