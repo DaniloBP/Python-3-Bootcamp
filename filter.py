82d769a059f98ee8b304cf7b99acc9940c6d83e9
Danilo Batista <pereira.b.danilo@gmail.com>
2019-04-04 15:45:30 -0300

    More about filters

--- filter.py
+++ filter.py
@@ -10,8 +10,26 @@ users = [
 		  { "user_name" : "Paul_George", "shopcart" : ["Basket Ball NIKE", "T-shirt 76ers"]},
 		  { "user_name" : "KittyKat", "shopcart" : []},
 		  { "user_name" : "Rainbow_Killer", "shopcart" : ["Pro Bow", "Silver Arrows"]},
-		  { "user_name" : "GodsSlayer", "shopcart" : []}
+		  { "user_name" : "GodsSlayer", "shopcart" : []},
+		  { "user_name" : "T'chala", "shopcart" : []},
+		  { "user_name" : "Sam", "shopcart" : []}
 		]
 
-buyers = list( filter( lambda user : not user["shopcart"], users) )
-print(buyers)
+# Using filters.
+empty_shopcarts = list( filter( lambda user : not user["shopcart"], users) )
+# print(empty_shopcarts)
+
+# Using List Comprehension.
+empty_shopcarts2 = [user["user_name"].upper() for user in users if not user["shopcart"] ]
+# print(empty_shopcarts2)
+
+# Using Maps combined w/ Filters.
+short_names = list( map( lambda user : user["user_name"].upper(), filter( lambda user : not user["shopcart"], users ) ) )
+# print(short_names)
+
+# ----------------------- EXERCISE ------------------------------
+def remove_negatives(numbers):
+    return list( filter( lambda num : num >= 0, numbers) )
+
+print( remove_negatives([-1,3,4,-99]) )	# [3,4] 
+print(remove_negatives([-12,-1,0,24,-58,45,77,32]) )		# [0,24,45,77,32]
